76a3484648349002dc22bcaf87d8cdcf334d8371
Matthew3721 <changjun030110@gmail.com>
2026-05-21 17:12:46 +0800

    test on price micro distridution

--- process_data.py
+++ process_data.py
@@ -3,11 +3,24 @@ import numpy as np
 from pathlib import Path
 from collections import deque
 
-def process_data(inputfile, outputfile, alpha = 0.0001):
+def process_data(inputfile, outputfile, label_method, FE: bool = True,alpha = 0.0001):
+    '''
+    label_method: 'l1 - direct price move: pt+10 - pt'; 'l2 - m+t smooth: (m+t - pt)/pt'; 'l3 - m+t and m-t smooth: (m+t - m-t)/m-t'
+    FE: 'True - Conduct feature engineering'; 'False - No feature engineering'
+    '''
+
+    label_methods = ['l1', 'l2', 'l3']
+    if label_method not in label_methods:
+        print('Please check label_method input')
+        return None
+    if (FE != True) and (FE != False):
+        print('Please check FE input')
+        return None
+    
     # calculate the micro price for each level, and add it as a new column to the dataframe
     def price_micro(df, level: int):
         price_cols = [f'bid{level}', f'ask{level}']
-        size_cols = [f'bidSize{level}', f'askSize{level}']   
+        size_cols = [f'bidcumSize{level}', f'askcumSize{level}']
         df[f'price_micro_{level}'] = (df[price_cols[0]] * df[size_cols[1]] + df[price_cols[1]] * df[size_cols[0]]) / (df[size_cols[0]] + df[size_cols[1]])
         return None
     
@@ -23,20 +36,36 @@ def process_data(inputfile, outputfile, alpha = 0.0001):
     # read the csv file, only keep the timestamp and the 20 levels of price and volume
     df = pd.read_csv(inputfile, usecols = [0] + list(range(5,25)))
 
+    for i in range(1,6):
+        df[f'askcumSize{i}'] = df[f'askSize{i}'] + (df[f'askcumSize{i-1}'] if i != 1 else 0)
+        df[f'bidcumSize{i}'] = df[f'bidSize{i}'] + (df[f'bidcumSize{i-1}'] if i != 1 else 0)
+
     # Caculate the mid price
     df['MidPrice'] = (df['bid1'] + df['ask1']) / 2
 
-    # calculate the micro price for each level
-    for i in range(1,6):
-        price_micro(df, level = i)
-    
-    # select the timestamp, mid price and micro price columns
-    df_selected = pd.concat([df.iloc[:, [0]], df.iloc[:, -6:]], axis=1)
+    if FE == False:
+        df_selected = df
+    elif FE == True:
+        # calculate the micro price for each level
+        for i in range(1,6):
+            price_micro(df, level = i)
+        
+        # select the timestamp, mid price and micro price columns
+        df_selected = pd.concat([df.iloc[:, [0]], df.iloc[:, -6:]], axis=1)
 
     # calculate the price move and price move percentage
-    m_plus_t = df_selected['MidPrice'].rolling(window=10).mean().shift(-10)
-    df_selected['price_move'] = (m_plus_t - df_selected['MidPrice'])
-    df_selected['price_move_pctg'] = df_selected['price_move'] / df_selected['MidPrice']
+    if label_method == 'l1':
+        df['price_move'] = (df_selected['MidPrice'].shift(-10) - df_selected['MidPrice'])
+        df_selected['price_move_pctg'] = df_selected['price_move'] / df_selected['MidPrice']
+    elif label_method == 'l2':
+        m_plus_t = df_selected['MidPrice'].rolling(window=10).mean().shift(-10)
+        df_selected['price_move'] = (m_plus_t - df_selected['MidPrice'])
+        df_selected['price_move_pctg'] = df_selected['price_move'] / df_selected['MidPrice']
+    elif label_method == 'l3':
+        m_plus_t = df_selected['MidPrice'].rolling(window=10).mean().shift(-10)
+        m_minus_t = df_selected['MidPrice'].rolling(window=10).mean().shift(10)
+        df_selected['price_move'] = m_plus_t - m_minus_t
+        df_selected['price_move_pctg'] = df_selected['price_move'] / m_minus_t
     
     # calculate the price move label
     df_selected['price_move_label'] = df_selected['price_move_pctg'].apply(lambda x: move_label(x, alpha))
@@ -44,19 +73,28 @@ def process_data(inputfile, outputfile, alpha = 0.0001):
     # drop the rows with NaN values
     df_selected.dropna(inplace=True)
 
+    cols = df_selected.columns.to_list()
+    cols.remove('MidPrice')
+    cols.insert(1, 'MidPrice')
+    df_selected = df_selected[cols]
+
     # save the processed data to a new csv file
     df_selected.to_csv(outputfile, index=False)
     
     print(f"Data processing completed. Processed data saved to {outputfile}.")
 
-def window_normalize(inputpath, outputpath, window_size = 5):
+def window_normalize(inputpath, outputpath, FE: bool = True, window_size = 5):
     # read the processed csv file
     folder = Path(inputpath)
     csv_files = sorted(folder.glob('*.csv'))
 
     buffer = deque(maxlen=window_size + 1)
 
-    cols_to_keep = [2, 3, 4, 5, 6, 9]
+    if FE == True:
+        cols_to_keep = [2, 3, 4, 5, 6, 9]
+    elif FE == False:
+        cols_to_keep = [n for n in range(2,22)]
+        cols_to_keep.append(24)
     processed_rows = 0
     
     for file in csv_files:
@@ -71,7 +109,7 @@ def window_normalize(inputpath, outputpath, window_size = 5):
             stats_df = pd.concat(stats_list, ignore_index=True)
 
             target_df = buffer[-1]
-            feature_cols = [col for col in target_df.columns if col.startswith('price_micro')]
+            feature_cols = [col for col in target_df.columns if not col.startswith('price_move')]
             processed_rows += len(target_df)
 
             hist_mean = stats_df[feature_cols].mean()
@@ -92,16 +130,17 @@ if __name__ == "__main__":
     path = Path(outputpath)
     path.mkdir(parents=True, exist_ok=True)
     alpha = 0.00005
+    FE = True
 
     raw_files = list(Path(inputpath).glob('*.csv'))
 
     for raw_file in raw_files:
         print(f'Processing file:{raw_file.name}')
         outputfile = f'{outputpath}processed_{raw_file.stem[-4:]}.csv'
-        process_data(raw_file, outputfile, alpha)
+        process_data(raw_file, outputfile, label_method='l2', FE = FE, alpha = alpha)
 
     inputpath_normalized = 'Data/Processed_data/'
     outputpath_normalized = 'Data/Normalized_data/' 
     path = Path(outputpath_normalized)
     path.mkdir(parents=True, exist_ok=True)
-    window_normalize(inputpath_normalized, outputpath_normalized, window_size = 5)
+    window_normalize(inputpath_normalized, outputpath_normalized, FE = FE, window_size = 5)
